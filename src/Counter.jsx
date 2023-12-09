288856f66c196503cecee3a537e23bd08c5adcba
Parth Miroliya <parthmiroliya@gmail.com>
2023-12-09 18:18:28 +0530

    Counrt

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -32,7 +32,7 @@ class Counter extends Component {
                     <h1>Counter App </h1>
 
                     <button onClick={this.Increment} style={{padding : "5px 30px", fontSize : "28px" , borderRadius : "10px", border : "none", backgroundColor : "teal", color : "white"}}>+</button>
-                    <button style={{display : "inline-block", margin : "10px", padding : "5px 30px", fontSize : "28px" , borderRadius : "10px", border : "none", backgroundColor : "lightblue"}}>{this.state.cnt}</button>
+                    <button style={{display : "inline-block", margin : "10px", padding : "5px 30px", fontSize : "28px" , borderRadius : "10px", border : "none", backgroundColor : "lightblue"}}>{this.state.cnt}</button> 
                     <button onClick={this.Decrement} style={{padding : "5px 30px", fontSize : "28px", borderRadius : "10px", border : "none", backgroundColor : "teal", color : "white"}}>-</button>
                     <button onClick={this.Reset} style={{display : "block",margin : "20px 10px", padding : "5px 30px", fontSize : "26px" , borderRadius : "10px", border : "none", backgroundColor : "teal", color : "white"}}>reset</button>
                 </center>
